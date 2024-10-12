b793c067c3419f732c22186b4b284f1c373b3eb1
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2024-10-12 20:16:09 +0300

    checkstyle fix #1

--- hw14-spring-integrations/src/main/java/ru/otus/hw/service/JourneyServiceImpl.java
+++ hw14-spring-integrations/src/main/java/ru/otus/hw/service/JourneyServiceImpl.java
@@ -7,14 +7,14 @@ import java.util.Random;
 
 @Service
 public class JourneyServiceImpl implements JourneyService {
-    private static final int survivalChance = 70;
+    private static final int SURVIVAL_CHANCE = 70;
 
     @Override
     public Adventurer magicalBomb(Adventurer adventurer) {
         System.out.println("Взрыв магической бомбы");
         Random random = new Random();
         int dice = random.nextInt(0, 101);
-        if (dice > survivalChance) {
+        if (dice > SURVIVAL_CHANCE) {
             adventurer.setAlive(false);
             System.out.println("Герой погиб от взрыва магической бомбы...");
         }
